d6d12bdbd0c0110985e16f9086a6c746469f3061
Abid Al-Wasi <abidalwasi@outlook.com>
2021-03-28 23:56:08 +0600

    updated project

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ window.onload = () => {
   
   let inSalary = 0;
   let inAge = 0;
+  let counter = 0;
   
   for(i = 0; i < table.rows.length; i++) {
     let salary = parseInt(table.rows[i].cells[3].innerHTML);
     let age = parseInt(table.rows[i].cells[4].innerHTML);
-    
     inSalary = inSalary + salary;
     inAge = inAge + age;
+    counter++;
   }
   
   salaryElement.innerHTML = inSalary;
   ageElement.innerHTML = inAge;
+  console.log(counter);
   // console.log("Total Salary: " + inSalary + " & " + "Total Age: " + inAge);
 }
